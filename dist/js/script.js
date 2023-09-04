0f30f4a2e3fd87c3758c55037653249c591b4d91
Yarik <slavtuk.slavtuk2003@gmail.com>
2023-09-05 02:10:01 +0300

    added new sections

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,3 +9,11 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.skills__ratings-counter');
+const lines = document.querySelectorAll('.skills__ratings .skills__ratings-line span');
+
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
